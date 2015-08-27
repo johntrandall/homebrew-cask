c36534f0f172305b25b3573c6b7e26a14d29308b
John Randall <john@johnrandall.com>
2015-08-27 11:06:11 -0400

    add synergy

---
+++ Casks/Synergy.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'Synergy' do
+  version '1.7.4'
+  sha256 '4cd0c7973caf0f5e77a6b24e2e89bfc7f41594cbd2c8bf5ad0b7c569052c91cc'
+
+  url 'http://mandrillapp.com/track/click/30640799/synergy-project.org?p=eyJzIjoiMHN6WmhDX29ET0lYd1NsUHYyMHdTM0NmQXM0IiwidiI6MSwicCI6IntcInVcIjozMDY0MDc5OSxcInZcIjoxLFwidXJsXCI6XCJodHRwOlxcXC9cXFwvc3luZXJneS1wcm9qZWN0Lm9yZ1xcXC9hY2NvdW50XFxcL3Jlc2V0XFxcLz9yZXR1cm49JTJGZG93bmxvYWQlMkYmdG9rZW49ZmUyMTczMWU5NjY0ZGM1M2E0NmZlNmZkNDNjNTg5ZTNlYzUyZTNmMlwiLFwiaWRcIjpcIjhlNzQwNDY1NGFiYTQwMWI4YWM1YjI4MWZmMDBiZTU3XCIsXCJ1cmxfaWRzXCI6W1wiZGY4NDAwMDc0M2E4MzVkN2VmMjExNzVhMTViNmNjZTc3MGQ2NWJjN1wiXX0ifQ'
+  name 'Synergy'
+  homepage 'http://mandrillapp.com'
+  license :commercial    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Synergy.app'
+end
